4dbd670f58df478d3f2c31f3f726105e5b1c4a69
LitenFlyg <118855263+LitenFlyg@users.noreply.github.com>
2024-05-21 10:36:19 +0200

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -161,6 +161,7 @@ st.markdown("""
 &nbsp;
 &nbsp;
 &nbsp;
+<hr style='border:2px solid black'> 
 <div style='text-align: center;'><span style='font-size:50px;'>&#8595;</span></div>
 &nbsp;
 """, unsafe_allow_html=True)
