e4dcb9bbca766d416f9c2ca025505f018ab7c97f
Brandon Nilsson <118360174+BarreBN@users.noreply.github.com>
2024-05-21 15:52:45 +0200

    changed so the picture has a dark mode behaviour

--- Application/app.py
+++ Application/app.py
@@ -113,7 +113,7 @@ topimage_base64 = image_to_base64(topimage_path)
 st.markdown(
     f"""
     <div style="display: flex; justify-content: center;">
-        <img src="data:image/png;base64,{topimage_base64}" style="height: 500px;">
+        <img src="data:image/png;base64,{topimage_base64}" class="topimage" style="height: 500px;">
     </div>
     """,
     unsafe_allow_html=True
