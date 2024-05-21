c752507f12148ceab706c3bd30220fcd8c4a6d8d
LitenFlyg <118855263+LitenFlyg@users.noreply.github.com>
2024-05-21 12:28:15 +0200

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -114,7 +114,7 @@ st.markdown(
     f"""
     <div style="display: flex; align-items: center;">
         <img src="data:image/png;base64,{topdescimage_base64}" style="height: 200px; margin-right: 20px;">
-        <p style="font-family: Arial; font-size: 14px;">
+        <p style="font-family: Arial; font-size: 20px;">
         1. Upload your job posting in either .txt or .docx format.<br>
         2. Adjust the parameters in the sidebar to match your ideal candidate's profile.<br>
         3. Click 'Run' to get AI-generated recommendations for improving your job posting.
@@ -126,7 +126,7 @@ st.markdown(
 
 st.markdown(
     """
-    <h1 style="text-align: center; font-size: 40px;"><span style="font-family: Papyrus;">CoRecruit</span> AI</h1>
+    <h1 style="text-align: center; font-size: 60px;"><span style="font-family: Papyrus;">CoRecruit</span> AI</h1>
     """,
     unsafe_allow_html=True
 )
@@ -135,9 +135,9 @@ st.markdown(
 # Display the logo in the sidebar
 st.sidebar.markdown(
     f"""
-    <div style="display: flex; align-items: left;">
+    <div style="display: flex; align-items: center;">
         <img src="data:image/png;base64,{logo_base64}" style="height: 120px; margin-right: 15px;">
-        <h1 style="text-align: center; font-size: 30px;"><span style="font-family: Papyrus;">CoRecruit</span> AI</h1>
+        <h1 style="text-align: left; font-size: 30px;"><span style="font-family: Papyrus;">CoRecruit</span> AI</h1>
     </div>
     <hr style='border:1px solid black'> 
     """,
