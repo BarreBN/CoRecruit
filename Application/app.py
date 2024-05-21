ab43dcd4f52645b838bea1e42a14f386acf6b828
LitenFlyg <118855263+LitenFlyg@users.noreply.github.com>
2024-05-21 11:50:18 +0200

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -110,7 +110,7 @@ topdescimage_base64 = image_to_base64(topdescimage_path)
 # Main page content
 st.markdown(
     f"""
-    <div style="display: flex; align-items: center;">
+    <div style="display: flex; flex-direction: column; align-items: center;">
         <img src="data:image/png;base64,{topdescimage_base64}" style="height: 200px; margin-right: 15px;">
         <h1 style="display: inline;">CoRecruit AI</h1>
     </div>
